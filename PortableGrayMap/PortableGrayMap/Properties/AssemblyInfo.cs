0b5f9caa3421af3054398ae1e4bf0b4d01ed943a
David <hichaeretaqua@web.de>
2015-02-14 14:32:15 +0100

    Added shared assembly version and switched to version 0.1

--- PortableGrayMap/PortableGrayMap/Properties/AssemblyInfo.cs
+++ PortableGrayMap/PortableGrayMap/Properties/AssemblyInfo.cs
@@ -2,36 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("PortableGrayMap")]
 [assembly: AssemblyDescription("Load and saves files in formats as pgm, ppm, pbm.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Tanpop")]
-[assembly: AssemblyProduct("PortableGrayMap")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5d953cb1-8561-485a-8e13-5682fbd0a304")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
 [assembly: InternalsVisibleTo("PortableGrayMapTest")]
