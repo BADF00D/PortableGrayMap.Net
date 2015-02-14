d255a6da950c225c59191f8e6d368af6aa82ef63
David <hichaeretaqua@web.de>
2015-02-14 16:35:06 +0100

    Able to read P4 images.

---
+++ PortableGrayMap/PortableGrayMap/Reader/P4Reader.cs
@@ -0,0 +1,42 @@
+﻿namespace PortableGrayMap.Reader
+{
+    using System.Collections.Generic;
+    using System.IO;
+
+    internal class P4Reader : BasicReader<IPortbaleBitmap, bool>
+    {
+        protected override string ExpectedMagicNumber
+        {
+            get { return "P4"; }
+        }
+
+        protected override IPortbaleBitmap ContinueReadImage(BinaryReader reader, int width, int height)
+        {
+            var pixels = new bool[width*height];
+            var index = 0;
+
+            SkipWhiteSpaceCharacters(reader);
+
+            for (var i = 0; i < pixels.Length/8; i++)
+            {
+                var @byte = reader.ReadByte();
+                EncodeByteAndStoreDate(pixels, @byte, index);
+                index += 8;
+            }
+
+            return new PortbaleBitmap(width, height, pixels);
+        }
+
+        private static void EncodeByteAndStoreDate(IList<bool> pixels, byte b, int index)
+        {
+            pixels[index + 0] = (b & 128) > 0;
+            pixels[index + 1] = (b & 64) > 0;
+            pixels[index + 2] = (b & 32) > 0;
+            pixels[index + 3] = (b & 16) > 0;
+            pixels[index + 4] = (b & 8) > 0;
+            pixels[index + 5] = (b & 4) > 0;
+            pixels[index + 6] = (b & 2) > 0;
+            pixels[index + 7] = (b & 1) > 0;
+        }
+    }
+}
---
+++ PortableGrayMap/PortableGrayMapTest/Reader/P4ReaderTest.cs
@@ -0,0 +1,66 @@
+﻿namespace PortableGrayMapTest.Reader
+{
+    using System;
+    using System.IO;
+    using NUnit.Framework;
+    using PortableGrayMap;
+    using PortableGrayMap.Reader;
+    using Tanpohp.Extensions;
+
+    [TestFixture]
+    public class P4ReaderTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            _reader = new P4Reader();
+        }
+
+        private const string PathToSampleFolder = @"../../../Sources/Samples/P4/";
+
+        private IPortbaleMapReader<IPortbaleBitmap, bool> _reader;
+
+        [Test]
+        [ExpectedException(typeof (ArgumentException))]
+        public void CheckMagicNumber()
+        {
+            _reader.ReadFromFile(Path.Combine(PathToSampleFolder, "../P1/", "feep.ascii.pbm"));
+        }
+
+        [Test]
+        public void CheckSizeOfSampleCircle()
+        {
+            var image = _reader.ReadFromFile(Path.Combine(PathToSampleFolder, "circle.pbm"));
+
+            Assert.AreEqual(200, image.Width, "Width");
+            Assert.AreEqual(200, image.Height, "Height");
+            Assert.IsNotNull(image.Pixels, "Pixels");
+            Assert.AreEqual(40000, image.Pixels.Length, "Number of Pixels");
+            //test some pixels
+            Assert.IsFalse(image.Pixels[0], "Pixel 0");
+            Assert.IsFalse(image.Pixels[29*200 + 99], "Pixel 29*200+99");
+            Assert.IsTrue(image.Pixels[30*200 + 99], "Pixel 30*200+99");
+            Assert.IsTrue(image.Pixels[35*200 + 99], "Pixel 35*200+99");
+            Assert.IsFalse(image.Pixels[36*200 + 99], "Pixel 36*200+99");
+            Assert.IsFalse(image.Pixels[199*200 + 199], "Pixel 199*200+99");
+        }
+
+        [Test]
+        public void CrossCheckWithP1Reader()
+        {
+            var imageFromP4Reader = _reader.ReadFromFile(Path.Combine(PathToSampleFolder, "circle.pbm"));
+            var imageFromP1Reader =
+                new P1Reader().ReadFromFile(Path.Combine(PathToSampleFolder, "../P1/", "circle.ascii.pbm"));
+
+            Assert.AreEqual(imageFromP1Reader.Width, imageFromP4Reader.Width, "Width");
+            Assert.AreEqual(imageFromP1Reader.Height, imageFromP4Reader.Height, "Height");
+            for (var y = 0; y < 200; y++)
+            {
+                for (var x = 0; x < 200; x++)
+                {
+                    Assert.AreEqual(imageFromP1Reader[x, y], imageFromP4Reader[x, y], "Pixel {0}/{1}".FormatWith(x, y));
+                }
+            }
+        }
+    }
+}
