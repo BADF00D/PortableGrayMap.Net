72447d34b8df777adb69207d4f19f56316f80f91
David <hichaeretaqua@web.de>
2015-02-14 15:51:27 +0100

    Moved commonly used part of P1Reader to BasicReader.

---
+++ PortableGrayMap/PortableGrayMap/Reader/BasicReader.cs
@@ -0,0 +1,102 @@
+﻿namespace PortableGrayMap.Reader
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using Tanpohp.Extensions;
+
+    internal abstract class BasicReader<TMap, TPixel> : IPortbaleMapReader<TMap, TPixel>
+        where TMap : IPortbleAnymap<TPixel>
+    {
+        protected abstract string ExpectedMagicNumber { get; }
+
+        public virtual TMap ReadFromStream(Stream stream)
+        {
+            if (stream == null) throw new ArgumentNullException("stream");
+            if (!stream.CanRead) throw new ArgumentException("Cannot read from stream.");
+
+            var reader = new BinaryReader(stream, Encoding.ASCII);
+            var magicNumber = ReadMagicNumber(reader);
+            if (magicNumber != ExpectedMagicNumber)
+                throw new ArgumentException("MagicNumberFor '{0}' differs from expected '{1}'".FormatWith(magicNumber,
+                    ExpectedMagicNumber));
+            var width = ReadNextInt(reader);
+            var height = ReadNextInt(reader);
+
+            return ContinueReadImage(reader, width, height);
+        }
+
+
+        public TMap ReadFromFile(string path)
+        {
+            if (!File.Exists(path)) throw new ArgumentException("File does not extists.");
+            using (var fileStream = new FileStream(path, FileMode.Open))
+            {
+                return ReadFromStream(fileStream);
+            }
+        }
+
+        /// <summary>
+        /// Continues to read partial read image. Readers position is directly after HEIGHT in file.
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        protected abstract TMap ContinueReadImage(BinaryReader reader, int width, int height);
+
+        /// <summary>
+        /// Reads from reader as long is current caracter is a digit and returns the whole number.
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        protected static int ReadNextInt(BinaryReader reader)
+        {
+            SkipWhiteSpaceCharacters(reader);
+
+            var rawString = new StringBuilder();
+            while (char.IsDigit((char) reader.PeekChar()))
+            {
+                rawString.Append(reader.ReadChar());
+            }
+
+            return Int32.Parse(rawString.ToString());
+        }
+
+        /// <summary>
+        /// Skips seeks reader to next non whitespace. If it find a comment, this is also skipped.
+        /// </summary>
+        /// <param name="reader"></param>
+        protected static void SkipWhiteSpaceCharacters(BinaryReader reader)
+        {
+            var currentChar = (char) reader.PeekChar();
+            while (char.IsWhiteSpace(currentChar))
+            {
+                reader.ReadChar();
+                currentChar = (char) reader.PeekChar();
+                while (currentChar == '#')
+                {
+                    SkipCommentLine(reader);
+                    currentChar = (char) reader.PeekChar();
+                }
+            }
+        }
+
+        private static void SkipCommentLine(BinaryReader reader)
+        {
+            var currentChar = (char) reader.PeekChar();
+            while (currentChar != '\r' && currentChar != '\n')
+            {
+                currentChar = reader.ReadChar();
+            }
+        }
+
+        private static string ReadMagicNumber(BinaryReader reader)
+        {
+            var buffer = new char[2];
+            reader.Read(buffer, 0, 2);
+
+            return buffer[0] + "" + buffer[1];
+        }
+    }
+}
--- PortableGrayMap/PortableGrayMap/Reader/P1Reader.cs
+++ PortableGrayMap/PortableGrayMap/Reader/P1Reader.cs
@@ -1,25 +1,16 @@
 ﻿namespace PortableGrayMap.Reader
 {
-    using System;
     using System.IO;
-    using System.Text;
-    using Tanpohp.Extensions;
 
-    internal class P1Reader : IPortbaleMapReader<IPortbaleBitmap, bool>
+    internal class P1Reader : BasicReader<IPortbaleBitmap, bool>
     {
-        private const string MagicNumberForP1 = "P1";
-
-
-        public IPortbaleBitmap ReadFromStream(Stream stream)
+        protected override string ExpectedMagicNumber
         {
-            if(stream == null) throw new ArgumentNullException("stream");
-            if(!stream.CanRead) throw new ArgumentException("Cannot read from stream.");
+            get { return "P1"; }
+        }
 
-            var reader = new BinaryReader(stream, Encoding.ASCII);
-            var magicNumber = ReadMagicNumber(reader);
-            if (magicNumber != MagicNumberForP1) throw new ArgumentException("MagicNumberFor '{0}' differs from expected '{1}'".FormatWith(magicNumber, MagicNumberForP1));
-            var width = ReadNextInt(reader);
-            var height = ReadNextInt(reader);
+        protected override IPortbaleBitmap ContinueReadImage(BinaryReader reader, int width, int height)
+        {
             var pixels = ReadPixels(reader, width, height);
 
             return new PortbaleBitmap(width, height, pixels);
@@ -37,61 +28,6 @@
             }
 
             return pixels;
-        }
-
-        private static int ReadNextInt(BinaryReader reader)
-        {
-            SkipWhiteSpaceCharacters(reader);
-
-            var rawString = new StringBuilder();
-            while (char.IsDigit((char) reader.PeekChar()))
-            {
-                rawString.Append(reader.ReadChar());
-            }
-
-            return Int32.Parse(rawString.ToString());
-        }
-
-        private static void SkipWhiteSpaceCharacters(BinaryReader reader)
-        {
-            var currentChar = (char)reader.PeekChar();
-            while (char.IsWhiteSpace(currentChar))
-            {
-                reader.ReadChar();
-                currentChar = (char)reader.PeekChar();
-                while (currentChar == '#')
-                {
-                    SkipCommentLine(reader);
-                    currentChar = (char)reader.PeekChar();
-                }
-            }
-        }
-
-        private static void SkipCommentLine(BinaryReader reader)
-        {
-            var currentChar = (char) reader.PeekChar();
-            while (currentChar != '\r' && currentChar != '\n')
-            {
-                currentChar = reader.ReadChar();
-            }
-        }
-
-        private static string ReadMagicNumber(BinaryReader reader)
-        {
-            var buffer = new char[2];
-            reader.Read(buffer, 0, 2);
-
-            return buffer[0] + "" + buffer[1];
-        }
-
-        public IPortbaleBitmap ReadFromFile(string path)
-        {
-            
-            if(!File.Exists(path)) throw new ArgumentException("File does not exists.");
-            using (var stream = new FileStream(path, FileMode.Open))
-            {
-                return ReadFromStream(stream);
-            }
         }
     }
 }
