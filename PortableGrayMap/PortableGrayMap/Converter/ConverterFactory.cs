f0d5c06d3e732116efef52ab3292d7b30f4e95ef
David <hichaeretaqua@web.de>
2015-02-19 18:50:57 +0100

    Added converter to ConverterFactory.

--- PortableGrayMap/PortableGrayMap/Converter/ConverterFactory.cs
+++ PortableGrayMap/PortableGrayMap/Converter/ConverterFactory.cs
@@ -6,21 +6,36 @@
 
     public static class ConverterFactory
     {
-        private static readonly Lazy<IPortableBitmapToConverter<Bitmap>> PortableBitmapToBitmapConvert =
+        private static readonly Lazy<IPortableBitmapToConverter<Bitmap>> PbmToBitmapConverter =
             new Lazy<IPortableBitmapToConverter<Bitmap>>(() => new PortableBitmapToBitmapConverter());
 
-        private static readonly Lazy<IPortableBitmapToConverter<BitmapSource>> PortableBitmapToBitmapSourceConverter =
+        private static readonly Lazy<IPortableBitmapToConverter<BitmapSource>> PbmToBitmapSourceConverter =
             new Lazy<IPortableBitmapToConverter<BitmapSource>>(() => new PortableBitmapToBitmapSourceConverter());
 
+        private static readonly Lazy<IPortableGraymapToConverter<BitmapSource>> PgmToBitmapSourceConverter =
+            new Lazy<IPortableGraymapToConverter<BitmapSource>>(() => new PortableGraymapToBitmapSourceConverter());
+
+        private static readonly Lazy<IPortableGraymapToConverter<Bitmap>> PgmToBitmapConverter =
+            new Lazy<IPortableGraymapToConverter<Bitmap>>(() => new PortableGraymapToBitmapConverter());
 
         public static IPortableBitmapToConverter<BitmapSource> ToBitmapSourceConverter
         {
-            get { return PortableBitmapToBitmapSourceConverter.Value; }
+            get { return PbmToBitmapSourceConverter.Value; }
         }
 
         public static IPortableBitmapToConverter<Bitmap> ToBitmapConverter
         {
-            get { return PortableBitmapToBitmapConvert.Value; }
+            get { return PbmToBitmapConverter.Value; }
+        }
+
+        public static IPortableGraymapToConverter<BitmapSource> PortableGraymapToBitmapSourceConverter
+        {
+            get { return PgmToBitmapSourceConverter.Value; }
+        }
+
+        public static IPortableGraymapToConverter<Bitmap> PortableGraymapToBitmapConverter
+        {
+            get { return PgmToBitmapConverter.Value; }
         }
     }
 }
