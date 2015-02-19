4c586e219bb160a7624ee01290ccedcaddc96695
David <hichaeretaqua@web.de>
2015-02-19 17:46:59 +0100

    Added converter for bitmaps to ConverterFactory.

--- PortableGrayMap/PortableGrayMap/Converter/ConverterFactory.cs
+++ PortableGrayMap/PortableGrayMap/Converter/ConverterFactory.cs
@@ -2,18 +2,25 @@
 {
     using System;
     using System.Drawing;
+    using System.Windows.Media.Imaging;
 
     public static class ConverterFactory
     {
-        private static readonly Lazy<IPortableBitmapToConverter<Bitmap>> PortableBitmaptoBitmapConvert =
+        private static readonly Lazy<IPortableBitmapToConverter<Bitmap>> PortableBitmapToBitmapConvert =
             new Lazy<IPortableBitmapToConverter<Bitmap>>(() => new PortableBitmapToBitmapConverter());
 
-        public static IPortableBitmapToConverter<T> GetConvertFor<T>()
-        {
-            if (typeof (T) == typeof (Bitmap))
-                return (IPortableBitmapToConverter<T>) PortableBitmaptoBitmapConvert.Value;
+        private static readonly Lazy<IPortableBitmapToConverter<BitmapSource>> PortableBitmapToBitmapSourceConverter =
+            new Lazy<IPortableBitmapToConverter<BitmapSource>>(() => new PortableBitmapToBitmapSourceConverter());
 
-            throw new ArgumentException("Unkown destination format.");
+
+        public static IPortableBitmapToConverter<BitmapSource> ToBitmapSourceConverter
+        {
+            get { return PortableBitmapToBitmapSourceConverter.Value; }
+        }
+
+        public static IPortableBitmapToConverter<Bitmap> ToBitmapConverter
+        {
+            get { return PortableBitmapToBitmapConvert.Value; }
         }
     }
 }
